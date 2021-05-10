59dc3b8cf1ea7ad6ca67d7ad68a08bd41fa3dc4b
Andreea Muresan <amuresan99@gmail.com>
2021-05-10 19:55:53 +0300

    completed image blending

--- OpenCVApplication.cpp
+++ OpenCVApplication.cpp
@@ -97,16 +97,16 @@ Mat_<Vec3i> convertVec3bToInt(Mat_<Vec3b> matrix) {
 	return result;
 }
 
-vector<Mat_<Vec3b>> genGauss(Mat_<Vec3b> img, int levels) {
+vector<Mat_<Vec3b>> genGauss(Mat_<Vec3b> img, int levels) { 
 
-	vector<Mat_<Vec3b>> gaussPyramid;
+	vector<Mat_<Vec3b>> gaussPyramid; 
 	Mat_<Vec3b> level;
 
 	gaussPyramid.push_back(img);
 
 	for (int i = 0; i < levels; i++) {
 
-		pyrDown(gaussPyramid[i], level);
+		pyrDown(gaussPyramid[i], level); 
 		gaussPyramid.push_back(level);
 	}
 	return gaussPyramid;
@@ -122,7 +122,7 @@ void testGauss(int noOfLayers) {
 			x += std::to_string(i);
 			cv::resize(gaussianPyr[i], gaussianPyr[i], cv::Size(256, 256));
 			imshow(x, gaussianPyr[i]);
-
+			
 		}
 
 		waitKey();
@@ -133,7 +133,7 @@ vector<Mat_<Vec3i>> genLaplace(Mat_<Vec3b> src, int levels) {
 
 	vector<Mat_<Vec3i>> aux;
 	vector<Mat_<Vec3b>> gaussPyramid = genGauss(src, levels);
-
+	
 	aux.push_back(convertVec3bToInt(gaussPyramid.back()));
 	for (int i = gaussPyramid.size() - 1; i >= 1; --i) {
 
@@ -182,7 +182,7 @@ void testLaplace(int layers) {
 			printLaplace(laplacianPyr[i], x);
 			cv::resize(src, src, cv::Size(256, 256));
 		}
-
+		
 		imshow("image", src);
 	}
 }
@@ -198,27 +198,12 @@ Mat_<Vec3b> reconstructImgFromLapPyr(vector<Mat_<Vec3i>> lapPyr) {
 	}
 	return image;
 }
-void testReconstruction(int layers) {
-	char fname[MAX_PATH];
-	while (openFileDlg(fname)) {
-		Mat src;
-		src = imread(fname, IMREAD_COLOR);
 
-		std::vector<Mat_<Vec3i>> laplacianPyr = genLaplace(src, layers);
-		Mat_<Vec3b> rec = reconstructImgFromLapPyr(laplacianPyr);
 
-		imshow("Diference", (rec - src) * 10 + 128);
+float *compute_MAE(Mat_<Vec3b> firstImage, Mat_<Vec3b> secondImage) {
 
-		imshow("reconstructed", rec);
-		imshow("image", src);
-		waitKey(0);
-	}
-}
-
-float* compute_MAE(Mat_<Vec3b> firstImage, Mat_<Vec3b> secondImage) {
-
-	float* MAE = (float*)calloc(3, sizeof(float));
-	for (int i = 0; i < firstImage.rows; i++)
+	float *MAE = (float*)calloc(3,sizeof(float));
+	for(int i=0;i<firstImage.rows;i++)
 		for (int j = 0; j < firstImage.cols; j++) {
 			MAE[0] = MAE[0] + abs(firstImage(i, j)[0] - secondImage(i, j)[0]);
 			MAE[1] = MAE[1] + abs(firstImage(i, j)[1] - secondImage(i, j)[1]);
@@ -231,6 +216,129 @@ float* compute_MAE(Mat_<Vec3b> firstImage, Mat_<Vec3b> secondImage) {
 	return MAE;
 }
 
+void testReconstruction(int layers) {
+	char fname[MAX_PATH];
+	while (openFileDlg(fname)) {
+		Mat src;
+		src = imread(fname, IMREAD_COLOR);
+
+		std::vector<Mat_<Vec3i>> laplacianPyr = genLaplace(src, layers);
+		Mat_<Vec3b> rec = reconstructImgFromLapPyr(laplacianPyr);
+		float* MAE = compute_MAE(src, rec);
+		std::cout << MAE[0] << " " << MAE[1] << " " << MAE[2] << endl;
+
+		imshow("Diference", (rec - src) * 10 + 128);
+
+		imshow("reconstructed", rec);
+		imshow("image", src);
+
+		waitKey(0);
+	}
+}
+void merge() {
+
+	Mat_<Vec3b> src1 = imread("images/apple.jpg", IMREAD_COLOR);
+	Mat_<Vec3b> src2 = imread("images/orange.jpg", IMREAD_COLOR);
+
+	Mat_<Vec3b> g1 = src1.clone();
+	Mat_<Vec3b> g2 = src2.clone();
+
+
+	std::vector<Mat_<Vec3b>> gpA;
+	gpA.push_back(g1);
+	for (int i = 0; i < 6; i++)
+	{
+		pyrDown(gpA[i], g1);
+		gpA.push_back(g1);
+	}
+
+	std::vector<Mat_<Vec3b>> gpB;
+	gpB.push_back(g2);
+	for (int i = 0; i < 6; i++)
+	{
+		pyrDown(gpB[i], g2);
+		gpB.push_back(g2);
+	}
+
+
+	std::vector<Mat_<Vec3b>> lpA;
+	lpA.push_back(gpA[5]);
+	for (int i = 5; i > 0; i--)
+	{
+		Mat_<Vec3b> ge;
+		Mat_<Vec3b> l;
+		Size size = Size(gpA[i - 1].cols, gpA[i - 1].rows);
+		pyrUp(gpA[i], ge, size);
+		subtract(gpA[i - 1], ge, l);
+		lpA.push_back(l);
+	}
+
+
+	std::vector<Mat_<Vec3b>> lpB;
+	lpB.push_back(gpB[5]);
+	for (int i = 5; i > 0; i--)
+	{
+		Mat_<Vec3b> ge;
+		Mat_<Vec3b> l;
+		Size size = Size(gpB[i - 1].cols, gpB[i - 1].rows);
+		pyrUp(gpB[i], ge, size);
+		subtract(gpB[i - 1], ge, l);
+		lpB.push_back(l);
+	}
+
+	std::vector<Mat_<Vec3b>> LS;
+
+	for (int i = 0; i < 6; i++)
+	{
+		Mat_<Vec3b> ls(lpA[i].rows, lpA[i].cols);
+		for (int j = 0; j < lpA[i].rows; j++)
+		{
+			for (int k = 0; k < lpA[i].cols; k++)
+			{
+				if (k < (lpA[i].cols / 2))
+				{
+					ls(j, k) = lpA[i](j, k);
+				}
+				else
+				{
+					ls(j, k) = lpB[i](j, k);
+				}
+			}
+		}
+		LS.push_back(ls);
+	}
+
+	Mat_<Vec3b> ls_ = LS[0];
+	for (int i = 1; i < 6; i++)
+	{
+		Size size = Size(LS[i].cols, LS[i].rows);
+		pyrUp(ls_, ls_, size);
+		add(ls_, LS[i], ls_);
+	}
+
+	imshow("Pyramid blending", ls_);
+
+	Mat_<Vec3b> real(src1.rows, src1.cols);
+	for (int j = 0; j < src1.rows; j++)
+	{
+		for (int k = 0; k < src1.cols; k++)
+		{
+			if (k < (src1.cols / 2))
+			{
+				real(j, k) = src1(j, k);
+			}
+			else
+			{
+				real(j, k) = src2(j, k);
+			}
+		}
+	}
+
+	imshow("Direct connection", real);
+
+	
+	waitKey();
+}
 
 
 int main()
@@ -245,7 +353,7 @@ int main()
 		printf(" 1 - Calculate n levels for Gaussian Pyramid .\n");
 		printf(" 2 - Calculate n levels for Laplace Pyramid .\n");
 		printf(" 3 - Reconstruct the image .\n");
-		printf(" 4 - Calculate the mean absolute error .\n");
+		printf(" 4 - Apple and orange bleending.\n");
 		printf("Option: ");
 		scanf("%d", &op);
 		switch (op)
@@ -265,7 +373,12 @@ int main()
 			scanf("%d", &n);
 			testReconstruction(n);
 			break;
+		case 4: 
+			merge();
+			break;
+
 		}
-	} while (op != 0);
+
+	} 	while (op != 0);
 	return 0;
 }
